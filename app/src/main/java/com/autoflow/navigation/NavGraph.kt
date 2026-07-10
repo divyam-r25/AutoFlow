8b8e2e7ddd5b0171166f4bb9e231c289882b516e
divyam-r25 <divyamrathore254@gmail.com>
2026-07-10 17:59:56 +0530

    build Autoflow v1

--- app/src/main/java/com/autoflow/navigation/NavGraph.kt
+++ app/src/main/java/com/autoflow/navigation/NavGraph.kt
@@ -1,7 +1,7 @@
 package com.autoflow.navigation
 
 import androidx.compose.runtime.Composable
-import com.autoflow.ui.screens.HomeScreen
+import com.autoflow.features.home.HomeScreen
 
 @Composable
 fun NavGraph() {
